7eef3a9caf8711b3ec26996ac07bf5e66dfbcc41
caleb-grover <caleb@alphaomegapc.com>
2026-05-13 17:28:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function toggleMenu() {
 
         // Restart animation
         menu.style.animation = "none";
-        void menu.offsetWidth; // forces reflow
+        void menu.offsetWidth; 
         menu.style.animation = "fadeInMenu 0.35s ease-out";
     }
 }
@@ -22,18 +22,25 @@ function closeMenu() {
     document.getElementById("centerMenu").style.display = "none";
 }
 
-
-function closeMenu() {
-    document.getElementById("menuOverlay").style.display = "none";
-    document.getElementById("centerMenu").style.display = "none";
-}
-
-
 function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
+}
+
+// First-time popup
+window.onload = function() {
+    if (!localStorage.getItem("visitedBefore")) {
+        document.getElementById("welcomeOverlay").style.display = "block";
+        document.getElementById("welcomePopup").style.display = "block";
+        localStorage.setItem("visitedBefore", "true");
+    }
+};
+
+function closeWelcome() {
+    document.getElementById("welcomeOverlay").style.display = "none";
+    document.getElementById("welcomePopup").style.display = "none";
 }
 
 console.log("Caleb's music site is running!");
